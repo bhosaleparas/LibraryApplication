e8ebe8fce7a28e2c0557968c74a93894d322a287
bhosaleparas <parasbhosale375@gmail.com>
2025-12-19 14:14:23 +0530

    disable csrf protection,made api stateless,removed springboot built login methods

--- src/main/java/com/example/LibraryApplication/config/WebSecurity.java
+++ src/main/java/com/example/LibraryApplication/config/WebSecurity.java
@@ -1,10 +1,15 @@
 package com.example.LibraryApplication.config;
 
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,13 +17,34 @@ public class WebSecurity {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.
-                authorizeHttpRequests(auth->auth
+        httpSecurity
+                .csrf(csrfConfig -> csrfConfig.disable())
+                .sessionManagement(sessionConfig ->
+                        sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth->auth
                         .requestMatchers("/api/users/**").permitAll()
-                        .requestMatchers("/api/books/**").authenticated()
-                )
-                .formLogin(Customizer.withDefaults());
+                        .requestMatchers("/api/books/**").permitAll()
+                        .requestMatchers("/api/borrow-records/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
+                );
+//                .formLogin(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
 }
